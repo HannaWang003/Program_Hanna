80cc279d75f505cafa886d51825925fe679e1d1c
Hanna <hannawang003@gmail.com>
2023-10-02 10:06:10 +0800

    第1.2節課

--- P20231002/index.php
+++ P20231002/index.php
@@ -12,7 +12,15 @@
 $age = 30;
 echo $age;
 $name =  "Hanna";
-echo $name;
+echo $name.'<hr>';
+// 練習: 交換2個變數的值
+$a=15;
+$b=43;
+echo '原本<br>$a='.$a.'<br>$b='.$b.'<hr>';
+$c=$b;
+$b=$a;
+$a=$c;
+echo '<br>交換後<br>$a='. $a.'<br>$b='.$b;
 
 ?>
 </body>
