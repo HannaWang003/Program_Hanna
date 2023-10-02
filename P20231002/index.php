e6ea2eaf82c22964abc25525349f9156c4cf1f41
Hanna <hannawang003@gmail.com>
2023-10-02 09:13:36 +0800

    php第一個網頁

---
+++ P20231002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+$age = 30;
+echo $age;
+$name =  "Hanna";
+echo $name;
+
+?>
+</body>
+
+</html>
