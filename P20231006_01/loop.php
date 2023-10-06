23a2b5d79cb1418aa8f68ae466fa6b42ba0becd7
Hanna <hannawang003@gmail.com>
2023-10-06 09:30:53 +0800

    for迴圈

--- P20231006_01/loop.php
+++ P20231006_01/loop.php
@@ -2,6 +2,12 @@
 $sum=0;
 for($i=0;$i<=100;$i=$i+1){
     $sum=$sum+$i;
+    if($sum%2>0){
+echo $sum."*<br>";
+    }
+    else{
+        echo $sum."XX<br>";
+    }
 }
 echo "1加到100的結果是" . $sum;
 ?>
