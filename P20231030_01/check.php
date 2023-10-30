2101f7fe0e610eaedf1c27dc5828b2eedfb2f5af
Hanna <hannawang003@gmail.com>
2023-10-30 12:16:31 +0800

    session/cookie

---
+++ P20231030_01/check.php
@@ -0,0 +1,13 @@
+<?php
+// session_start();
+if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+    // $_SESSION['login']=$_POST['acc'];
+    setcookie("login","hanna",time()+300);
+    header("location:member.php");
+
+}else{
+    setcookie("error","帳號或密碼錯誤,請重新登入",time()+5);
+    // $_SESSION['error']="帳號或密碼錯誤,請重新登入";
+    header("location:login.php");
+}
+?>
