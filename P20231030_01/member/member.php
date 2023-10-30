dac260e96317b5b29ec9b5eff7ee07e090b07fc8
Hanna <hannawang003@gmail.com>
2023-10-30 14:18:33 +0800

    session: member.php

--- P20231030_01/member/member.php
+++ P20231030_01/member/member.php
@@ -11,19 +11,17 @@
 <body>
     <?php
     include_once('header.php');
-    date_default_timezone_set("Asia/Taipei");
-    // session_start();
-    if(isset($_COOKIE['login']) && !empty($_COOKIE['login'])){
-    echo "<h3>登入成功</h3>";
-    echo "<a href='login.php'>回登入頁</a>";
-    echo "<a href='logout.php'>登出</a>";
-}else{
-    setcookie("error","沒有登入相關驗證，非法登入",time()+5);
-    // $_SESSION['error']="沒有登入相關驗證，非法登入";
-    header("location:login.php");
-}
-
-?>
+    session_start();
+    if(isset($_SESSION['login']) && !empty($_SESSION['login'])){
+        echo "<h3>登入成功</h3>";
+        echo "<a href='login.php'>回登入頁</a>";
+        echo "<a href='logout.php'>登出</a>";
+    }else{
+        $_SESSION['error']="沒有登入相關驗證，非法登入";
+       header("location:login.php");
+    }
+    
+    ?>
 
 </body>
 
