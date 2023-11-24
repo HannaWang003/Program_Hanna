922f3c80339aea8b8196f0eaa86b868ae99685bb
Hanna <hannawang003@gmail.com>
2023-11-24 09:52:24 +0800

    feat:宜告一個class並實例化

--- 1121124_php_object/index.php
+++ 1121124_php_object/index.php
@@ -1,1 +1,15 @@
 <?php
+class Animal
+{
+    public $name;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
+
+$animal = new Animal; //實例化 instant
+
+$animal->setName('小花');
+echo $animal->name;
