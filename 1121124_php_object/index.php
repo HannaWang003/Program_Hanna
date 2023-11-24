c1c7d8d70d648311b9b6e4dea9453fa423068c64
Hanna <hannawang003@gmail.com>
2023-11-24 11:02:26 +0800

    feat:介紹繼承

--- 1121124_php_object/index.php
+++ 1121124_php_object/index.php
@@ -1,15 +1,37 @@
 <?php
 class Animal
 {
-    public $name;
+    private $name;
+
+    public function __construct($name) //被new出來之前就開始執行了
+    {
+        $this->name = $name;
+    }
 
     public function setName($name)
     {
         $this->name = $name;
     }
+    public function getName()
+    {
+        return $this->name;
+    }
 }
+// $animal = new Animal('小明'); //實例化 instant
 
-$animal = new Animal; //實例化 instant
+// $animal->setName('小花');
+// echo $animal->getName();
 
-$animal->setName('小花');
-echo $animal->name;
+class Dog extends Animal
+{
+    function sit()
+    {
+        echo "坐下";
+    }
+}
+$dog = new Dog('汪汪');
+echo $dog->getName();
+echo "<br>";
+$dog->setName('阿旺');
+echo $dog->getName();
+echo $dog->sit();
