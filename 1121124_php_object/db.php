2523ba099d4ad248283a46bdfc2cbf1f2e1d0242
Hanna <hannawang003@gmail.com>
2023-11-28 08:46:06 +0800

    refactor:del_total()

--- 1121124_php_object/db.php
+++ 1121124_php_object/db.php
@@ -66,25 +66,6 @@ class DB
             echo "錯誤:沒有指定的資料表名稱";
         }
     }
-    // function_2_回傳資料庫有沒有核對到資料，並回傳筆數
-    function total($id)
-    {
-        $sql = "select count(`id`) from `$this->table` ";
-
-        if (is_array($id)) {
-            foreach ($id as $col => $value) {
-                $tmp[] = "`$col`='$value'";
-            }
-            $sql .= " where " . join(" && ", $tmp);
-        } else if (is_numeric($id)) {
-            $sql .= " where `id`='$id'";
-        } else {
-            echo "錯誤:參數的資料型態比須是數字或陣列";
-        }
-        //echo 'find=>'.$sql;
-        $row = $this->pdo->query($sql)->fetchColumn();
-        return $row;
-    }
 
     //function_3_篩選出特定資料
     function find($id)
--- 112117_php-function/test.php
+++ 112117_php-function/test.php
@@ -1,35 +1,35 @@
 <?php
-del('dept',['id'=>'13','code'=>'113']);
+del('dept', ['id' => '13', 'code' => '113']);
 
 
 
 // pdo
-function pdo($host,$dbname){
-    $dns="mysql:host=$host;charset=utf8;dbname=$dbname";
-    return $pdo=new PDO($dns,'root','');
+function pdo($host, $dbname)
+{
+    $dns = "mysql:host=$host;charset=utf8;dbname=$dbname";
+    return $pdo = new PDO($dns, 'root', '');
 }
 // foreach
-function where($id){
-    foreach($id as $col => $val){
-        $tmp[]="`$col`='$val'";
+function where($id)
+{
+    foreach ($id as $col => $val) {
+        $tmp[] = "`$col`='$val'";
     }
     return $tmp;
 }
 // 6-del
-function del($table,$id){
-$pdo=pdo('localhost','school');
-    $sql="delete from `$table` where ";
-    if(is_array($id)){
-$tmp=where($id);
-$sql .= join(" && ",$tmp);
-    }else if(is_numeric($id)){
- echo $sql .=" `id`='$id'";
-    }
-    else{
+function del($table, $id)
+{
+    $pdo = pdo('localhost', 'school');
+    $sql = "delete from `$table` where ";
+    if (is_array($id)) {
+        $tmp = where($id);
+        $sql .= join(" && ", $tmp);
+    } else if (is_numeric($id)) {
+        echo $sql .= " `id`='$id'";
+    } else {
         echo "錯誤:參數錯誤!";
     }
 
-return $pdo->exec($sql);
-
+    return $pdo->exec($sql);
 }
-?>
